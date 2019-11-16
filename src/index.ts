80ce64c66613e47c2ed16eb53d1f2b02b5685a13
Mike Eling <mike.eling97@gmail.com>
2019-11-16 01:36:33 +0100

    :wrench: Refactoring and fixes

--- src/index.ts
+++ src/index.ts
@@ -5,92 +5,129 @@ import * as tempDirectory from "temp-dir";
 import * as util from "util";
 import { v1 as uuid } from "uuid";
 
+// tslint:disable-next-line: no-var-requires
 const pjson = require("../package.json");
+// tslint:disable-next-line: no-var-requires
 const exec = util.promisify(require("child_process").exec);
 
 class ReactInstant extends Command {
-  public static description = "describe the command here";
+  public static description = "Launches preview of a remote React project.";
 
   public static flags = {
     port: flags.integer({ char: "p", description: "Custom port." }),
-    save: flags.string({ char: "s", description: "Provide a url to save the project permanenty." }),
+    save: flags.string({
+      char: "s",
+      description: "Provide a url to save the project permanenty."
+    }),
     verbose: flags.boolean(),
     version: flags.version({ char: "v" })
   };
 
   public static args = [{ name: "git_url" }];
 
-  public verbose = false;
-  public prefersYarn = false;
-  public saveProjectPath: string | undefined = undefined;
+  private verbose = false;
+  private prefersYarn = false;
+  private dir = "";
 
-  public async checkDependencies() {
+  /**
+   * Here it all begins...
+   */
+  public async run() {
+    this.log(`react-instant v${pjson.version}\n`);
+    const { args, flags: flgs } = this.parse(ReactInstant);
+
+    this.verbose = flgs.verbose;
+
+    this.dir = flgs.save || `${tempDirectory}/react-instant-${uuid()}`;
+    this.verboseLog(`Path was set to ${this.dir}`);
+
+    if (!args.git_url) {
+      throw new Error("No git url provided");
+    }
+
+    await this.checkDependencies();
+    await this.cloneRepo(args.git_url);
+    await this.installDeps();
+    await this.buildRepo();
+    await this.serveRepo(flgs.port || 5000);
+  }
+
+  /**
+   * Checks if required packages are installed.
+   */
+  private async checkDependencies() {
+    // Check for yarn
     if (commandExistsSync("yarn")) {
       this.prefersYarn = true;
       this.verboseLog("Default package manager set to yarn.");
     }
+    // Check for git
     if (!commandExistsSync("git")) {
       throw new Error("'git' is not installed.");
     }
+    // Check for serve
     if (!commandExistsSync("serve")) {
-      this.log("serve not found, installing...");
+      this.warn("serve not found, installing...");
       await exec(
         this.prefersYarn ? "yarn global add serve" : "npm install -g serve"
       );
     }
   }
 
-  public async cloneRepo(url: string) {
-    const tmpDir =
-      this.saveProjectPath || `${tempDirectory}/react-instant-${uuid()}`;
-
+  /**
+   * Clones git repository.
+   * @param url URL to repository.
+   */
+  private async cloneRepo(url: string) {
     !this.verbose
       ? this.log(`Cloning ${url}...`)
-      : this.verboseLog(`Cloning ${url} to ${tmpDir}...`);
-    this.verboseLog(await exec(`git clone ${url} ${tmpDir}`));
-    return tmpDir;
+      : this.verboseLog(`Cloning ${url} to ${this.dir}...`);
+
+    this.verboseLog(await exec(`git clone ${url} ${this.dir}`));
   }
 
-  public async installDeps(tmpDir: string) {
+  /**
+   * Installs project's dependencies.
+   */
+  private async installDeps() {
     this.log("Installing dependencies...");
+
     this.verboseLog(
-      await exec(`cd ${tmpDir} && ${this.prefersYarn ? "yarn" : "npm install"}`)
+      await exec(
+        `cd ${this.dir} && ${this.prefersYarn ? "yarn" : "npm install"}`
+      )
     );
   }
 
-  public async buildRepo(tmpDir: string) {
+  /**
+   * Builds project.
+   */
+  private async buildRepo() {
     this.log("Building project...");
+
     this.verboseLog(
-      await exec(`cd ${tmpDir} && ${this.prefersYarn ? "yarn" : "npm"} build`)
+      await exec(`cd ${this.dir} && ${this.prefersYarn ? "yarn" : "npm"} build`)
     );
   }
 
-  public async serveRepo(tmpDir: string, port: number) {
+  /**
+   * Serves project.
+   * @param port Served port.
+   */
+  private async serveRepo(port: number) {
     this.log("Serving project on port " + port + "...\n");
     this.log(`Now you can preview the project under http://localhost:${port}/`);
+
+    // tslint:disable-next-line: no-http-string
     await opn(`http://localhost:${port}/`, { url: true });
-    this.verboseLog(await exec(`cd ${tmpDir} && serve -s build -l ${port}`));
+
+    this.verboseLog(await exec(`cd ${this.dir} && serve -s build -l ${port}`));
   }
 
-  public async run() {
-    const { args, flags } = this.parse(ReactInstant);
-
-    this.verbose = flags.verbose;
-    this.saveProjectPath = flags.save;
-
-    if (!args.git_url) {
-      throw new Error("No git url provided");
-    }
-    this.log(`react-instant v${pjson.version}\n`);
-    this.verboseLog("Running in verbose mode\n");
-
-    await this.checkDependencies();
-    const tmpDir = await this.cloneRepo(args.gir_url);
-    await this.installDeps(tmpDir);
-    await this.buildRepo(tmpDir);
-    await this.serveRepo(tmpDir, flags.port || 5000);
-  }
-
+  /**
+   * Prints verbose log.
+   * @param args Output texts.
+   */
   private verboseLog(...args: any[]) {
     if (!this.verbose) {
       return;
