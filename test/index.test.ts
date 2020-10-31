37f548f9b719ff743b0329c4ad487e3c8e74b20b
Mike Eling <mike.eling97@gmail.com>
2020-10-31 02:33:55 +0100

    :white_check_mark: Exclude tests in CI
    Less work for the good robots working for GitHub :heart:

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,25 +7,25 @@ const cmd = require('../lib');
 describe('react-instant', () => {
   test
     .stdout()
-    .do(async () => await cmd.run(['https://github.com/DRFR0ST/physiomedica-website.git', "--omitServe"]))
+    .do(async () => await cmd.run(['https://github.com/DRFR0ST/physiomedica-website.git', "--omitServe", "--excludeTest"]))
     .exit(0)
     .it('from git url test exits with success.')
 
   test
     .stdout()
-    .do(async () => await cmd.run(['DRFR0ST/physiomedica-website', "--omitServe"]))
+    .do(async () => await cmd.run(['DRFR0ST/physiomedica-website', "--omitServe", "--excludeTest"]))
     .exit(0)
     .it('GitHub shorthand test exits with success.')
 
   test
     .stdout()
-    .do(async () => await cmd.run(['DRFR0ST/sourcer', "--omitServe", "--buildScript=compose"]))
+    .do(async () => await cmd.run(['DRFR0ST/sourcer', "--omitServe", "--buildScript=compose", "--excludeTest"]))
     .exit(0)
     .it('buildScript flag test exits with success.')
 
   test
     .stdout()
-    .do(async () => await cmd.run(['DRFR0ST/sourcer', "--omitServe", "--buildScript=glue"]))
+    .do(async () => await cmd.run(['DRFR0ST/sourcer', "--omitServe", "--buildScript=glue", "--excludeTest"]))
     .catch(err => expect(err.message).to.include('Command "glue" not found.'))
     .it('buildScript flag test exits with an error.')
 
